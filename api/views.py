78296ec830479b74259da906b50ffda511689946
yogendrasinghrawat <git.yrawat@gmail.com>
2022-06-01 10:53:39 +0530

    fetch data from backend to frontend

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse,JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import TaskSerializer
 from .models import Task
 
@@ -10,7 +11,7 @@ from .models import Task
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def see(request):
+def apiOverview(request):
     api_urls = {
         'List':'/task-list/',
         'Detail View':'/task-details/<str:pk>/',
@@ -25,6 +26,44 @@ def see(request):
 @permission_classes([IsAuthenticated])
 def taskList(request):
     tasks = Task.objects.all()
-    print(tasks)
-    serializers = TaskSerializer(tasks,many=True)
-    return Response(serializers.data)
+    serializer = TaskSerializer(tasks,many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def taskDetail(request,pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks,many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+    print(serializer.is_valid())
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors,status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def taskUpdate(request,pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task,many=False,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors,status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def taskDelete(request,pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    
+    return Response("Successfully deleted a task")
---
+++ frontend/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def list(request):
+    return render(request,"frontend/list.html")
